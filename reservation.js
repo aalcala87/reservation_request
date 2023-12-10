08c5d0a08a2fb0a834c071664931cf1a8af1e04b
aalcala87 <145299718+aalcala87@users.noreply.github.com>
2023-12-10 15:53:52 -0800

    Add files via upload

---
+++ reservation.js
@@ -0,0 +1,68 @@
+"use strict";
+
+$(document).ready(() => {
+    const emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/;
+
+    //Move focus to the “Arrival date” text box
+    $("#arrival_date").focus();
+
+    //Event handler for the submit event
+    $("#reservation_form").submit((event) => {
+        //Validate the user entries
+        const arrivalDate = $("#arrival_date").val().trim();
+        const nights = $("#nights").val().trim();
+        const email = $("#email").val().trim();
+        const phone = $("#phone").val().trim();
+
+        //Remove previous error messages
+        $(".error-message").remove();
+
+        //Check value is empty
+        if (arrivalDate === "") {
+            showError($("#arrival_date"), "Please enter an arrival date.");
+            //Cancel form submission
+			event.preventDefault();
+        }
+
+        if (nights === "") {
+            showError($("#nights"), "Please enter the number of nights.");
+            event.preventDefault();
+        }
+
+        //Check if the email address is not provided
+        if (email === "") {
+            showError($("#email"), "Please enter an email address.");
+            event.preventDefault();
+        } else {
+            //Check if the email address matches the pattern
+            if (!emailPattern.test(email)) {
+                showError($("#email"), "Please enter a valid email address.");
+                event.preventDefault();
+            }
+        }
+
+        //Check if the number of nights is numeric
+        if (isNaN(nights)) {
+            showError($("#nights"), "Number of nights must be numeric.");
+            event.preventDefault();
+        }
+
+        //Check if the phone number is not entered
+        if (phone === "") {
+            showError($("#phone"), "Please enter a phone number.");
+            event.preventDefault();
+        }
+
+        //Trim entries and put them back into the controls
+        $("#arrival_date").val(arrivalDate);
+        $("#nights").val(nights);
+        $("#email").val(email);
+        $("#phone").val(phone);
+    });
+
+    //Display error message next to the input field
+    function showError(element, message) {
+        const errorMessage = `<span class="error-message">${message}</span>`;
+        element.after(errorMessage);
+    }
+}); // end ready
