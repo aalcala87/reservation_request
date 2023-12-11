d83e63f0d06973d107affb7c6307e25bf118ef88
aalcala87 <145299718+aalcala87@users.noreply.github.com>
2023-12-10 16:08:20 -0800

    Update reservation.js

--- reservation.js
+++ reservation.js
@@ -3,7 +3,7 @@
 $(document).ready(() => {
     const emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/;
 
-    //Move focus to the “Arrival date” text box
+    //Move focus to the arrival date text box
     $("#arrival_date").focus();
 
     //Event handler for the submit event
@@ -13,6 +13,7 @@ $(document).ready(() => {
         const nights = $("#nights").val().trim();
         const email = $("#email").val().trim();
         const phone = $("#phone").val().trim();
+		const name = $("#name").val().trim();
 
         //Remove previous error messages
         $(".error-message").remove();
@@ -28,6 +29,11 @@ $(document).ready(() => {
             showError($("#nights"), "Please enter the number of nights.");
             event.preventDefault();
         }
+
+		if (name === "") {
+			showError($("#name"), "Please enter your name.");
+			event.preventDefault(); // Cancel form submission
+		}		
 
         //Check if the email address is not provided
         if (email === "") {
@@ -57,6 +63,7 @@ $(document).ready(() => {
         $("#arrival_date").val(arrivalDate);
         $("#nights").val(nights);
         $("#email").val(email);
+		$("#name").val(name);
         $("#phone").val(phone);
     });
 
